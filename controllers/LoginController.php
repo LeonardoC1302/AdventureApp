ce8186a8e4d25e09c6b086a8f5fe8973ac6fe13b
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-09-15 10:15:39 -0600

    Login & Register Routing

---
+++ controllers/LoginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Controllers;
+
+class LoginController{
+    public static function login(){
+        echo "Login";
+    }
+
+    public static function logout(){
+        echo "Logout";
+    }
+
+    public static function forgot(){
+        echo "Forgot Password";
+    }
+
+    public static function recover(){
+        echo "Recover Password";
+    }
+
+    public static function register(){
+        echo "Register";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,8 +3,26 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 
 $router = new Router();
+
+// Log In
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+
+$router->get('/logout', [LoginController::class, 'logout']);
+
+// Recover Password
+$router->get('/forgot', [LoginController::class, 'forgot']);
+$router->post('/forgot', [LoginController::class, 'forgot']);
+
+$router->get('/recover', [LoginController::class, 'recover']);
+$router->post('/recover', [LoginController::class, 'recover']);
+
+// Register
+$router->get('/register', [LoginController::class, 'register']);
+$router->post('/register', [LoginController::class, 'register']);
 
 
 
